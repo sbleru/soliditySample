1a79e9a6c2255a5156b0b44c8125642340a06679
sbleru <maruyama.shinji.1190@colopl.co.jp>
2018-06-24 09:43:18 +0900

    learn: Testing message updates

--- inbox/test/Inbox.test.js
+++ inbox/test/Inbox.test.js
@@ -31,4 +31,10 @@ describe('Inbox', () => {
         const message = await inbox.methods.message().call();
         assert.equal(message, "Hi there!");
     });
+
+    it('can change the message', async () => {
+        const log = await inbox.methods.setMessage('bye').send({ from: accounts[0] });
+        const message = await inbox.methods.message().call();
+        assert.equal(message, "bye");
+    });
 });
